188d465313478ac9e9dcb25c9fd55c0083dcec59
Alberto Leal <mail4alberto@gmail.com>
2014-04-26 15:48:54 -0400

    new stuffs

--- dist/game.js
+++ dist/game.js
@@ -91,28 +91,18 @@
 	
 	// Load images and sounds
 	main.prototype.preload = function() {
+	
+	    this.player = {};
+	    this.player.height = 92;
+	    this.player.width = 636/6;
+	
 	    this.game.load.image('ground', './assets/gfx/ground.png');
-	    this.game.load.spritesheet('player', './assets/gfx/avatar.png', 92, 90);
-	};
+	    this.game.load.spritesheet('players', './assets/gfx/avatar.png', this.player.width, this.player.height);
 	
-	// Setup the example
-	main.prototype.create = function() {
-	    // Set stage background to something sky colored
-	    // this.game.stage.backgroundColor = 0x4488cc;
-	
-	    // Create some ground for the player to walk on
-	    this.ground = this.game.add.group();
-	    for(var x = 0; x < this.game.width; x += 32) {
-	        // Add the ground blocks, enable physics on each, make them immovable
-	        var groundBlock = this.game.add.sprite(x, this.game.height - 32, 'ground');
-	        this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
-	        groundBlock.body.immovable = true;
-	        groundBlock.body.allowGravity = false;
-	        groundBlock.body.setSize(32, 20, 0, 12)
-	        this.ground.add(groundBlock);
-	    }
-	
-	
+	    // this.data = {};
+	    // this.data.player = {};
+	    // this.data.player.height = 105;
+	    // this.data.player.width = 92;
 	
 	    // Define movement constants
 	    this.MAX_SPEED = 250; // pixels/second
@@ -121,23 +111,68 @@
 	    this.GRAVITY = 980; // pixels/second/second
 	    this.JUMP_SPEED = -300; // pixels/second (negative y is up)
 	
-	    // Create a player sprite
-	    this.player = this.game.add.sprite(this.game.width/2, this.game.height - 150, 'player');
+	};
 	
-	    this.player.scale.setTo(0.7, 0.7);
+	// Setup the example
+	main.prototype.create = function() {
+	
+	    // Set stage background to something sky colored
+	    // this.game.stage.backgroundColor = 0x4488cc;
+	
+	    // Create some ground for the player to walk on
+	    this.ground = this.game.add.group();
+	    this.ground.data = {};
+	    this.ground.data.height = 32;
+	    this.ground.data.offset = 32/2;
+	
+	    var
+	    // ground
+	    ground_bound_offset = 32/2;
+	
+	    for(var x = 0; x < this.game.width; x += 32) {
+	
+	        // Add the ground blocks, enable physics on each, make them immovable
+	        var groundBlock = this.game.add.sprite(x, this.game.height - 32, 'ground');
+	
+	        this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
+	        groundBlock.body.immovable = true;
+	        groundBlock.body.allowGravity = false;
+	
+	        // change ground collision bounds
+	        groundBlock.body.setSize(32, 32 - ground_bound_offset, 0, ground_bound_offset);
+	
+	        this.ground.add(groundBlock);
+	    }
 	
 	
-	    // Enable physics on the player
-	    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+	    // Create players
+	    var player_pos_y = this.game.height - this.player.height - ground_bound_offset;
+	
+	    this.player.one = this.game.add.sprite(this.game.width/2, player_pos_y, 'players', 0);
+	    this.player.two = this.game.add.sprite(this.game.width/2-200, player_pos_y, 'players', 7);
+	
+	
+	
+	    // this.player_one.scale.setTo(0.7, 0.7);
+	    // this.player_two.scale.setTo(0.7, 0.7);
+	
+	
+	    // Enable physics on the players
+	    this.game.physics.enable(this.player.one, Phaser.Physics.ARCADE);
+	    this.game.physics.enable(this.player.two, Phaser.Physics.ARCADE);
+	
 	
 	    // Make player collide with world boundaries so he doesn't leave the stage
-	    this.player.body.collideWorldBounds = true;
+	    this.player.one.body.collideWorldBounds = true;
+	    this.player.two.body.collideWorldBounds = true;
+	
+	
 	
 	    // Set player minimum and maximum movement speed
-	    this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
+	    this.player.one.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
 	
 	    // Add drag to the player that slows them down when they are not accelerating
-	    this.player.body.drag.setTo(this.DRAG, 0); // x, y
+	    this.player.one.body.drag.setTo(this.DRAG, 0); // x, y
 	
 	    // Since we're jumping we need gravity
 	    this.game.physics.arcade.gravity.y = this.GRAVITY;
@@ -164,29 +199,36 @@
 	
 	// The update() method is called every frame
 	main.prototype.update = function() {
+	
+	
 	    if (this.game.time.fps !== 0) {
 	        this.fpsText.setText(this.game.time.fps + ' FPS');
 	    }
 	
 	    // Collide the player with the ground
-	    this.game.physics.arcade.collide(this.player, this.ground);
+	    this.game.physics.arcade.collide(this.player.one, this.ground);
+	    this.game.physics.arcade.collide(this.player.two, this.ground);
+	
+	    this.player.one.body.acceleration.x = -this.ACCELERATION;
+	
 	
 	    if (this.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
 	        // If the LEFT key is down, set the player velocity to move left
-	        this.player.body.acceleration.x = -this.ACCELERATION;
+	        this.player.one.body.acceleration.x = -this.ACCELERATION;
 	    } else if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
 	        // If the RIGHT key is down, set the player velocity to move right
-	        this.player.body.acceleration.x = this.ACCELERATION;
+	        this.player.one.body.acceleration.x = this.ACCELERATION;
 	    } else {
-	        this.player.body.acceleration.x = 0;
+	        this.player.one.body.acceleration.x = 0;
+	        // this.player.one.body.acceleration.x = -this.ACCELERATION;
 	    }
 	
 	    // Set a variable that is true when the player is touching the ground
-	    var onTheGround = this.player.body.touching.down;
+	    var onTheGround = this.player.one.body.touching.down;
 	
 	    if (onTheGround && this.input.keyboard.justPressed(Phaser.Keyboard.UP)) {
 	        // Jump when the player is touching the ground and the up arrow is pressed
-	        this.player.body.velocity.y = this.JUMP_SPEED;
+	        // this.player.body.velocity.y = this.JUMP_SPEED;
 	    }
 	};
 	
